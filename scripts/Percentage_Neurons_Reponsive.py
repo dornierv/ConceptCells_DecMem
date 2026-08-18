0590cf483185ecb5884caed6f9a08022c0493319
dornierv <dorniervincent0@gmail.com>
2026-08-18 11:57:42 +0200

    Script OK

--- scripts/Percentage_Neurons_Reponsive.py
+++ scripts/Percentage_Neurons_Reponsive.py
@@ -19,6 +19,10 @@ import mne
 from scipy import io as io
 from scipy import stats as stats
 
+
+# Inputs 
+path2data = 'C:/Users/dornier/GitHub/ConceptCells_DecMem/'
+
 ###########################################################################
 ############ FUNCTIONS USED LATER IN THE MAIN SCRIPT ######################
 ###########################################################################
@@ -60,9 +64,11 @@ def load_spikes(data_path):
     return spikes
 
 def get_firingrate(perievent):
-    bin_size = 0.2  # 200ms bin size
-    step_size = 0.01  # 10ms step size, to make overlapping bins
-    winsize = int(bin_size / step_size)  # Window size
+    # Parameters for firing rate estimation 
+    # Parameters are from pynapple documentation
+    bin_size = 0.2  
+    step_size = 0.01  
+    winsize = int(bin_size / step_size)  
 
 
     counts_im = perievent.count(step_size)
@@ -117,15 +123,13 @@ def average_neurons(path_json):
 
 
             # Path where data is stored
-            data_path = r'F:\Screening\database/'+bsnm+'/sess-'+str(session)
-            path_images = data_path+'/stimuli/' #"E:/screening hors eeg/Screening images/Pool images/"
+            data_path = path2data+'data/Examples_Session/'+bsnm+'/sess-'+str(session)+'/'
+            path_images = data_path+'/stimuli/' 
             
 
             # Load logfile
             logfname=data_path+'/lfps/'+bsnm+'_ses-01_task-Screening_run-01_ieeg_log.txt'
-
             logLines=np.array(read_lines(logfname, removeEndLines=True))
-
             stream=np.arange(len(logLines)/25, dtype=int)*25+1
             chRegs=np.array([line.split('.')[0] for line in logLines[stream]])
             print(chRegs,len(chRegs))
@@ -133,14 +137,6 @@ def average_neurons(path_json):
 
             # Load spiking activity from .nwb
             spikes = load_spikes(data_path)
-
-            # Load Run Lists and get TTL associated with each image
-            test_mat_Run1 = io.loadmat(path_images+'run-01.mat') # et concaténer les 4 runs
-            my_array_Run1 = test_mat_Run1['trial']
-            keys_TTL = [my_array_Run1[0,i][1][0][0] for i in range(my_array_Run1.shape[1])] 
-            values_images = [my_array_Run1[0,i][2][0] for i in range(my_array_Run1.shape[1])] 
-
-            
 
 
             # Load TTLs & dat files
@@ -164,12 +160,13 @@ def average_neurons(path_json):
             imgs=np.unique(TTLvals)
             imgIndices=[np.where(TTLvals==img_)[0] for img_ in imgs]
 
-
+            # Get the sample in which stimuli were presented
             imgTS=[TS_32768[imgIndice] for imgIndice in imgIndices]
 
             # Suppress the first value that correspond to TTL = 0 (i.e., fixation cross)
             imgTS = imgTS[1:]
 
+            # Extract list of neurons
             list_neurons = neurons_tp[iPatient][0][iSession]
 
             # Loop over neurons of interest
@@ -228,10 +225,10 @@ def average_neurons(path_json):
 ##################### MAIN SCRIPT #########################
 ###########################################################
 
-# Get the mean firing rate for all neurons in the region of interest
-nb_neuron_tp, nb_neuron_tp_responsive = average_neurons("C:/Users/dornier/GitHub/ConceptCells_TP/dictionary_singleunits_tp.json")
+# Get the mean firing rate for all neurons in the region of interest - Note that here is only for one session so not entire database
+nb_neuron, nb_neuron_responsive = average_neurons(path2data+"/data/Examples_Session/dictionary_singleunits_example.json")
 
 
-print('Il y a : '+str(nb_neuron_tp))
-print('\nDont '+str(nb_neuron_tp_responsive)+' qui répondent à au moins une image')
-print('\nSoit: '+str(nb_neuron_tp_responsive/nb_neuron_tp)+' pourcent de neurones')
+print('There are: '+str(nb_neuron) +' neurons')
+print('\nWith '+str(nb_neuron_responsive)+' that respond to at least one stimulus')
+print('\nSo: '+str(nb_neuron_responsive/nb_neuron)+' percent of neurons')
